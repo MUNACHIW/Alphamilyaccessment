bde9c1e834341eec534e15f1abbd2388c3130e4f
MUNACHIW <munasom25@gmail.com>
2025-03-04 07:30:57 +0100

    full setup and origin acceptance

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)  # Apply CORS to the Flask app
 
 # Set up the SQLite database
 basedir = os.path.abspath(os.path.dirname(__file__))
